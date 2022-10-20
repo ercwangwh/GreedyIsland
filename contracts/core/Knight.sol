7ab39df5d1111103c4d70c5e8b580533f25114ff
unknown <ercwangwh@gmail.com>
2022-10-20 12:09:44 +0800

    update knight.sol

--- contracts/core/Knight.sol
+++ contracts/core/Knight.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 // NFT faucet for testing
-contract Knight is ERC721, AccessControl {
+contract Knight is ERC721, AccessControl, ERC721Enumerable {
     using Counters for Counters.Counter;
-
+    using Strings for uint256;
+    string private constant _baseTokenURI =
+        "https://cloudflare-ipfs.com/ipfs/bafybeieifbwyhz4j3pf3l4ft2hfwfcygkcb3w2qp45sy2gxi2wh77kxbra/";
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
@@ -23,14 +28,46 @@ contract Knight is ERC721, AccessControl {
         _safeMint(to, tokenId);
     }
 
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // function setBaseURI(string memory baseURI_) external onlyOwner {
+    //     _baseTokenURI = baseURI_;
+    // }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
     // The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, AccessControl)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        // if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length != 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
     }
 }
